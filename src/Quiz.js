405522ce2cb8a67542df0f54c3485b64c8b6ddab
nathalieRobert <robert.nathaliejm@gmail.com>
2019-02-27 11:37:28 +0100

    "Completed module"

--- src/Quiz.js
+++ src/Quiz.js
@@ -9,11 +9,13 @@ class Quiz extends Component {
             this.state = { quiz_position: 1}
         }
         
-        render(){
-        return(
-        <div><div className = "QuizQuestion"> {quizData.quiz_questions[0].instruction_text}</div></div>
-        )
-    }
+       render(){
+           return(
+               <div><div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div></div>
+           )
+
+       }
+    
 
 }
 
