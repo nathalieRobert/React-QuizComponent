bf3320571438ced1c68ceef27f244621b06bdde8
nathalieRobert <robert.nathaliejm@gmail.com>
2019-02-27 11:20:36 +0100

    "Completed module"

---
+++ src/Quiz.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+
+let quizData = require('./quiz_data.json')
+
+class Quiz extends Component {
+
+        constructor(props) {
+            super(props)
+            this.state = { quiz_position: 1}
+        }
+
+        //div className appelle la question à l'index 0, "l'instruction de texte" (voir quiz_data.json)
+        render(){
+
+        return(
+        <div>
+            <div className = "QuizQuestion"> {quizData.quiz_questions[0].instruction_text} </div>
+        </div>
+        )
+    }
+
+}
+
+export default Quiz
