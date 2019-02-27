cd1d49acb44089ef4381d3cff5b3598157f82523
nathalieRobert <robert.nathaliejm@gmail.com>
2019-02-27 14:41:56 +0100

    "completed module"

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component {
         
         
        render(){
-        const isQuizEnd = false;
+        const isQuizEnd = true
            return(
                <div>
                    {isQuizEnd ? 
