32e170450b143e05e4074268a693fb2b5cbf2068
nathalieRobert <robert.nathaliejm@gmail.com>
2019-02-27 11:30:21 +0100

    "Completed module"

--- src/Quiz.js
+++ src/Quiz.js
@@ -8,12 +8,10 @@ class Quiz extends Component {
             super(props)
             this.state = { quiz_position: 1}
         }
-
-        //div className appelle la question à l'index 0, "l'instruction de texte" (voir quiz_data.json)
+        
         render(){
-
         return(
-        <div><div className = "QuizQuestion"> {quizData.quiz_questions[1].instruction_text}</div></div>
+        <div className = "QuizQuestion"> {quizData.quiz_questions[0].instruction_text}</div>
         )
     }
 
