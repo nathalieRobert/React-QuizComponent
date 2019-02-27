825c73287a648b3b83a6c22356f559339ad410eb
nathalieRobert <robert.nathaliejm@gmail.com>
2019-02-27 15:11:00 +0100

    "completed module"

--- src/Quiz.js
+++ src/Quiz.js
@@ -14,7 +14,7 @@ class Quiz extends Component {
         
         
        render(){
-        const isQuizEnd = true
+        const isQuizEnd = (this.state.quiz_position-1) === quizData.quiz_questions.lenght
            return(
                <div>
                    {isQuizEnd ? <QuizEnd/>:
