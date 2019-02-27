ef9277eea6cc7b3e894070c7f2aa965364fc3b5b
nathalieRobert <robert.nathaliejm@gmail.com>
2019-02-27 14:38:34 +0100

    "c m"

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -5,7 +5,7 @@ class QuizEnd extends Component{
         return(
             <div>
                 <p>Thanks for playing!</p>
-                <a ref=''>Reset Quiz</a>
+                <a href=''>Reset Quiz</a>
             </div>
         )
     }
